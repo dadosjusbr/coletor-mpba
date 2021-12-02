e0c2802cd8ae3d4ba609c9580e20b83fd6f8c6ce
joaolgm <joao.menezes@ccc.ufcg.edu.br>
2021-12-02 10:18:48 -0300

    altera caminho de testes

--- src/parser_test.py
+++ src/parser_test.py
@@ -9,12 +9,12 @@ class TestParser(unittest.TestCase):
     def test_jan_2020(self):
         self.maxDiff = None
         # Json com a saida esperada
-        with open("./output_test/test_parser/expected_2020.json", "r") as fp:
+        with open("src/output_test/test_parser/expected_2020.json", "r") as fp:
             expected_2020 = json.load(fp)
 
         files = [
-            "./output_test/test_parser/membros-ativos-contracheque-01-2020.ods",
-            "./output_test/test_parser/membros-ativos-verbas-indenizatorias-01-2020.ods",
+            "src/output_test/test_parser/membros-ativos-contracheque-01-2020.ods",
+            "src/output_test/test_parser/membros-ativos-verbas-indenizatorias-01-2020.ods",
         ]
 
         dados = load(files, "2020", "01")
@@ -26,10 +26,10 @@ class TestParser(unittest.TestCase):
     def test_jan_2018(self):
         self.maxDiff = None
         # Json com a saida esperada
-        with open("./output_test/test_parser/expected_2018.json", "r") as fp:
+        with open("src/output_test/test_parser/expected_2018.json", "r") as fp:
             expected_2018 = json.load(fp)
 
-        files = ["./output_test/test_parser/membros-ativos-contracheque-01-2018.ods"]
+        files = ["src/output_test/test_parser/membros-ativos-contracheque-01-2018.ods"]
 
         dados = load(files, "2018", "01")
         result_data = parse(dados, "mpba/01/2018", 1, 2018)
